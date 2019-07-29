4ac705ce246c736c25a837d812101fcaae022749
Austin McDaniel <amcdaniel2@gmail.com>
2019-07-29 08:22:38 -0400

    sb init and panda comp

---
+++ src/app/panda.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+// https://some-random-api.ml/img/panda
+// https://some-random-api.ml/facts/panda
+
+@Component({
+  selector: 'panda',
+  templateUrl: './panda.component.html',
+  styleUrls: ['./panda.component.scss']
+})
+export class PandaComponent {
+}
